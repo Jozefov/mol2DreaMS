256f25c2c1ea110497e8c40ad786247f34b21acb
Jozefov <mn.mastaf@gmail.com>
2024-10-09 17:21:13 +0200

    refactor

--- mol2dreams/scripts/train.py
+++ mol2dreams/scripts/train.py
@@ -2,19 +2,12 @@ import sys
 import yaml
 from mol2dreams.utils.parser import build_trainer_from_config
 
-
-
 def main(config_path):
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
 
-    # Build the trainer
     trainer = build_trainer_from_config(config)
-
-    # Start training
     trainer.train()
-
-    # Optionally test the model
     trainer.test()
 
 
