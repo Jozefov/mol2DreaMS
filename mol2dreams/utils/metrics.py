1680219135715a1a4000eebf19c545f41825a602
Jozefov <mn.mastaf@gmail.com>
2024-10-12 15:22:09 +0200

    rewritting cosine sim

--- mol2dreams/utils/metrics.py
+++ mol2dreams/utils/metrics.py
@@ -1,7 +1,6 @@
 from torch.nn import functional as F
 
 def cosine_similarity_metric(outputs, targets):
-    outputs_norm = F.normalize(outputs, p=2, dim=1)
-    targets_norm = F.normalize(targets, p=2, dim=1)
-    cosine_sim = (outputs_norm * targets_norm).sum(dim=1).mean()
-    return cosine_sim.item()
+
+    cosine_sim = F.cosine_similarity(outputs, targets, dim=1).mean().item()
+    return cosine_sim
